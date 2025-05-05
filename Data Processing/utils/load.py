bbfd753c927de3e8adf33fe63717faa615545cbd
Akhtar Jaffan Ramadhan <68815838+JustALearner101@users.noreply.github.com>
2025-05-05 14:32:19 +0700

    Delete Data Processing directory
    delete data directory

--- Data Processing/utils/load.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def load(data, filename="products.csv"):
-    with open(filename, mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["title", "price"])
-        writer.writeheader()
-        writer.writerows(data)
-    print(f"Data saved to {filename}")
